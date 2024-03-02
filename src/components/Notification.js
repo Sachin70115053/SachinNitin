20a2758808c0e0ea860f4ceca09ea04e54c70d0b
Sachin70115053 <161759249+Sachin70115053@users.noreply.github.com>
2024-03-03 00:01:32 +0530

    Add files via upload

---
+++ src/components/Notification.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Notification.css";
+import font from "./Notification_Images/font1.jpg"
+
+
+
+
+const Notification=()=>{
+    return(
+        <div className="Notification">
+            <img className="twenty" src={font}/>
+        </div>
+    )
+}
+
+export default Notification;
+
